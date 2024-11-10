291b8a933543c40975f557596f4f43e38062a7fe
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-11-10 13:22:25 -0500

    fix: consuming keycloak uri from config

--- gateway/src/main/java/micro/landmates/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/micro/landmates/gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package micro.landmates.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,23 +8,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder,
-            KeycloakAuthGatewayFilterFactory keycloakAuthFilter) {
-        return builder.routes()
-                // Specific route for user registration (with token filter)
-                .route("keycloak_user_registration_route", r -> r
-                        .path("/keycloak-server/admin/realms/landmates/users")
-                        .filters(f -> f
-                                .rewritePath("/keycloak-server/(?<segment>.*)", "/${segment}")
-                                .filter(keycloakAuthFilter.apply(new KeycloakAuthGatewayFilterFactory.Config())))
-                        .uri("http://localhost:9090"))
 
-                // General route without token filter for all other Keycloak paths
-                .route("keycloak_general_route", r -> r
-                        .path("/keycloak-server/**")
-                        .filters(f -> f.rewritePath("/keycloak-server/(?<segment>.*)", "/${segment}"))
-                        .uri("http://localhost:9090"))
-                .build();
-    }
+  @Value("${keycloak-url}")
+  private String keycloakUrl;
+
+  @Bean
+  public RouteLocator customRouteLocator(RouteLocatorBuilder builder,
+      KeycloakAuthGatewayFilterFactory keycloakAuthFilter) {
+    return builder.routes()
+        // Specific route for user registration (with token filter)
+        .route("keycloak_user_registration_route", r -> r
+            .path("/keycloak-server/admin/realms/landmates/users")
+            .filters(f -> f
+                .rewritePath("/keycloak-server/(?<segment>.*)", "/${segment}")
+                .filter(keycloakAuthFilter.apply(new KeycloakAuthGatewayFilterFactory.Config())))
+            .uri(keycloakUrl))
+
+        // General route without token filter for all other Keycloak paths
+        .route("keycloak_general_route", r -> r
+            .path("/keycloak-server/**")
+            .filters(f -> f.rewritePath("/keycloak-server/(?<segment>.*)", "/${segment}"))
+            .uri(keycloakUrl))
+        .build();
+  }
 }
